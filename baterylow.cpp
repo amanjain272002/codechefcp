ec42a75a1b2bee93d6ca01b1b651346cfb32a170
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-09-19 20:27:31 +0530

    commit

---
+++ baterylow.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    for (int i = 0;i<t;i++){
+        int x;
+        cin >> x ;
+        if(x<=15)
+        cout<<"YES";
+        else
+        cout<<"NO";
+        cout<<"\n";
+        
+    }
+    return 0;
+}
