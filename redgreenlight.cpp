3028c2b37b38d07737f77e6157371ddc1e4f8960
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-09-21 22:19:50 +0530

    commit

---
+++ redgreenlight.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    for (int i = 0;i<t;i++){
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin>>arr[i];
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if(arr[i]>k)
+            count++;
+        }
+        cout<<count<<endl;  
+    }
+    return 0;
+}
