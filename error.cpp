fc598e2496e8b4d725e86d0452dd5224c8a625c3
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-10-18 14:48:11 +0530

    commit

--- error.cpp
+++ error.cpp
@@ -1,34 +1,26 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
 using namespace std;
-int main(){
+int main()
+{
     int t;
-    cin>>t;
+    cin >> t;
     for (int i = 0; i < t; i++)
     {
-        string s;
-        int count0=0;
-        cin>>s;
-        for (int i = 0; i < s.length()-1; i++)
+        string s, ans = "Bad";
+        int count0 = 0;
+        cin >> s;
+        for (int i = 0; i < s.size() - 1; i++)
         {
-            if(s[i]!=s[i+1]){
-                count0++;
-            }
-            if(count0>=2){
+            if ((s[i] == '0' && s[i + 1] == '1' && s[i + 2] == '0') || (s[i] == '1' && s[i + 1] == '0' && s[i + 2] == '1'))
+            {
+                ans = "Good";
                 break;
             }
         }
-        if (count0==2)
-        {
-            cout<<"Good"<<endl;
-        }
-        else{
-            cout<<"Bad"<<endl;
-        }
-        
-        
+        cout << ans << endl;
     }
-    
+
     return 0;
 }
