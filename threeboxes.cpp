b6e01c0d65070cf0d97f13eb0f84c0843c917a68
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-10-21 20:55:48 +0530

    commit

--- threeboxes.cpp
+++ threeboxes.cpp
@@ -7,14 +7,27 @@ int main()
     cin >> t;
     for (int i = 0; i < t; i++)
     {
-        int a, b, c, d;
-        cin >> a >> b >> c >> d;
-        if (d >= c >= b >= a)
-            if (a + b + c == d)
-                cout << "1" << endl;
-            else if (a + b + c > d)
-                cout << floor((a + b + c) / 3) << endl;
+        int a[4],count = 1,temp;
+        for (int i = 0; i < 4; i++)
+        {
+            cin>>a[i];
+        }
+        temp = a[3];
+        for (int i = 0; i < 3; i++)
+        {
+            if (a[i]<=a[3])
+            {
+                a[3] = a[3] - a[i];
+            }
+            else{
+                count ++;
+                a[3] = temp;
+                a[3] = a[3] - a[i];
+            }
+        }
+        cout<<count<<endl;
     }
+    
 
     return 0;
 }
