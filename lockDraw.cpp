5b7a20b458d00cf5e428a060d18de4e9f4b45ad4
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-10-23 11:34:25 +0530

    commit

---
+++ lockDraw.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	int t;
+    cin>>t;
+    while (t--)
+    {
+        int a[3];
+        cin>>a[0]>>a[1]>>a[2];
+        sort(a,a+3);
+        if (a[0]+a[1] == a[2])
+        {
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    
+	return 0;
+}
