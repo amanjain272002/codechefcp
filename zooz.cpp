91834b46db5f41ff766607c4bb6dc32f2ee9a39e
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-10-28 19:46:23 +0530

    commit

---
+++ zooz.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        cout << "1";
+        for (int i = 1; i < n - 1; i++)
+        {
+            cout << "0";
+        }
+        cout << "1" << endl;
+    }
+
+    return 0;
+}
