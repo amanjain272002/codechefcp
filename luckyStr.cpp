302e0e3d29c59998869267f44e371d58806b197c
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-10-29 15:26:00 +0530

    commit

---
+++ luckyStr.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    long int k, n;
+    cin >> k >> n;
+    string a[k];
+    for (int i = 0; i < k; i++)
+    {
+        cin >> a[i];
+    }
+    for (long int i = 0; i < n; i++)
+    {
+        string s, str,ans = "BAD";;
+        cin >> s;
+
+        for (long int l = 0; l < s.length(); l++)
+        {
+            for (long int i = 0; i < k; i++)
+            {
+                if (s.length() >= 47 && s.length()<=50)
+                {
+                    ans = "GOOD";
+                    break;
+                }
+
+                else if (s.length() >= a[i].length())
+                {
+                    str = s.substr(l, a[i].length());
+                    if (str == a[i])
+                    {
+                        ans = "GOOD";
+                        break;
+                    }
+                }
+            }
+            if (ans == "GOOD")
+            {
+                break;
+            }
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
