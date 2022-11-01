296a7952bf01fdcacc5d2c04a31901f5b66a4261
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-11-01 21:24:43 +0530

    commit

---
+++ brokPhone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long int n,count = 0;
+        cin >> n;
+        long int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            
+        }
+        cout << count << endl;
+    }
+
+    return 0;
+}
