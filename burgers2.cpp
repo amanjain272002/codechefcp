14096dcd39d2c77c21241b7a409466820f871e0f
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-11-05 20:38:57 +0530

    commit

---
+++ burgers2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    long int t;
+    cin>>t;
+    while (t--)
+    {
+        long int x,y,n,r;
+        cin>>x>>y>>n>>r;
+        
+    }
+    
+    return 0;
+}
