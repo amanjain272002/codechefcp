73ed754c7b453eb4dd29dba8e2d35c30894bf68d
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-11-06 16:49:09 +0530

    commit

--- burgers2.cpp
+++ burgers2.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 int main(){
@@ -6,8 +7,19 @@ int main(){
     cin>>t;
     while (t--)
     {
-        long int x,y,n,r;
+        long int x,y,n,r,t;
         cin>>x>>y>>n>>r;
+        if (x*n>r)
+        {
+            cout<<"-1"<<endl;
+        }
+        else
+        {
+            t = (r-n*x)/(y-x);
+            t = min(n,t);
+            cout<<n-t<<" "<<t<<endl;
+        }
+        
         
     }
     
