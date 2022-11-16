e0bb9d5ee51cf5f109adffd34a675c97c79b4f86
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-11-16 17:03:25 +0530

    commit

--- brokPhone.cpp
+++ brokPhone.cpp
@@ -7,19 +7,28 @@ int main()
     cin >> t;
     while (t--)
     {
-        long int n,count = 0;
+        long int n,count = 0,w= 0 ,m = 0;
         cin >> n;
-        long int a[n];
+        long int a[100];
         for (int i = 0; i < n; i++)
         {
             cin >> a[i];
         }
 
-        for (int i = 0; i < n; i++)
+        for (int i = 1; i < n; i++)
         {
+            if (a[i] != a[i-1])
+            {
+                if (a[i-1] != a[0])
+                {
+                    w++;
+                }
+                m++;
+                
+            }
             
         }
-        cout << count << endl;
+        cout << w+m << endl;
     }
 
     return 0;
