46fbfd3dcab6a8c34427250853bc9885956091cd
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-11-25 21:30:46 +0530

    commit

---
+++ credScore.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;string ans = "NO";
+	cin>>x;
+	if(x>=750){
+	    ans = "YES";
+	}
+	cout<<ans<<endl;
+	return 0;
+}
