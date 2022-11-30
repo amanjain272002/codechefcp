636548a9f00184ddf3bbc7bd7ecb645a59e613a6
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-11-30 21:53:02 +0530

    commit

---
+++ name2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int j = 0,flag = 0;
+        string m,w;
+        cin>>m>>w;
+        
+    }
+    
+    return 0;
+}
