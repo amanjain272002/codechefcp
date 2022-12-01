c5d89950909807ff088d6098ccfacca2e28cf1ff
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-12-01 20:12:54 +0530

    commit

--- name2.cpp
+++ name2.cpp
@@ -1,17 +1,66 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
+int main()
+{
     int t;
-    cin>>t;
+    cin >> t;
     while (t--)
     {
-        int j = 0,flag = 0;
-        string m,w;
-        cin>>m>>w;
-        
+        int i = 0, j = 0, flag = 0;
+        string m, w, ans = "NO";
+        cin >> m >> w;
+        if (m.size() == w.size())
+        {
+            if (m == w)
+            {
+                ans = "YES";
+            }
+        }
+        else
+        {
+            if (m.size() < w.size())
+            {
+                while (i < m.size() && j < w.size())
+                {
+                    if (m[i] == w[j])
+                    {
+                        i += 1;
+                        j += 1;
+                    }
+                    else
+                    {
+                        j = j + 1;
+                    }
+                }
+                if (i == m.size())
+                {
+                    ans = "YES";
+                }
+            }
+            else
+            {
+                while (i < m.size() && j < w.size())
+                {
+                    if (m[i] == w[j])
+                    {
+                        i += 1;
+                        j += 1;
+                    }
+                    else
+                    {
+                        i = i + 1;
+                    }
+                }
+                if (j == w.size())
+                {
+                    ans = "YES";
+                }
+            }
+        }
+        cout << ans << endl;
     }
-    
+
     return 0;
 }
