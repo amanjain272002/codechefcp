594851fe1c770eed12ee53f7993dce322cdc0e3d
272002 <108273339+amanjain272002@users.noreply.github.com>
2022-12-06 21:39:48 +0530

    commit

---
+++ chbLLns.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int r,g,b,k;
+        cin>>r>>g>>b>>k;
+    }   
+    
+    return 0;
+}
