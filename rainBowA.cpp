80cdfc421222d9f4d04f12dbfac937c92114b15a
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-01-06 14:59:37 +0530

    commit

--- rainBowA.cpp
+++ rainBowA.cpp
@@ -1,6 +1,71 @@
 #include <iostream>
-#include <map>
 using namespace std;
+
+void solve(int n)
+{
+    int arr[n], f = 0, t, a = 1, z = 1;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > 7)
+        {
+            f = 1;
+            break;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if ((arr[i] > z && arr[i] != (z + 1)))
+        {
+            f = 1;
+            break;
+        }
+
+        else if (arr[i] > z && arr[i] == (z + 1))
+        {
+            z++;
+        }
+    }
+
+    if (n % 2 != 0)
+    {
+        t = (n + 1) / 2 - 1;
+        for (int i = 0; i < t; i++)
+        {
+            if (arr[i] != arr[n - i - 1])
+            {
+                f = 1;
+                break;
+            }
+        }
+    }
+    else
+    {
+        t = n / 2;
+        for (int i = 0; i < t; i++)
+        {
+            if (arr[i] != arr[n - i - 1])
+            {
+                f = 1;
+                break;
+            }
+        }
+    }
+
+    if ((f == 1) || (z < 7))
+    {
+        cout << "no" << endl;
+    }
+    else
+    {
+        cout << "yes" << endl;
+    }
+}
 
 int main()
 {
@@ -8,37 +73,9 @@ int main()
     cin >> t;
     while (t--)
     {
-        int n, a, flag = 0;
-        map<int, int> m;
+        int n;
         cin >> n;
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a;
-            m[a]++;
-        }
-
-        a = 1;
-        auto itr = m.begin();
-        for (auto it = m.begin(); it != m.end(); it++)
-        {
-            itr++;
-            if ((it->first != a || it->second % 2 != 0))
-            {
-                if (itr != m.end() || it->first != a)
-                {
-                    flag = 1;
-                }
-            }
-            a++;
-        }
-        if (flag == 1)
-        {
-            cout << "no" << endl;
-        }
-        else
-        {
-            cout << "yes" << endl;
-        }
+        solve(n);
     }
 
     return 0;
