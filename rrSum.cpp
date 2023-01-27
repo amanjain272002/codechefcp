000e35ceb02225280d863cace8823a2caf750899
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-01-27 20:52:44 +0530

    commit

---
+++ rrSum.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+    int n,m,q;
+    cin>>n>>m;
+    while (m--)
+    {
+        int count=0;
+        cin>>q;
+        if (q>n)
+        {
+            
+        }
+
+        cout<<count<<"\n";
+    }
+    
+    
+    return 0;
+}
