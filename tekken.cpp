3735b1dac3e39a0da86011af05a50394ebeee4e3
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-02-15 20:54:42 +0530

    commit

---
+++ tekken.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        int m = min(b, c);
+        b = b - m;
+        c = c - m;
+        if (a > b && a > c)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
