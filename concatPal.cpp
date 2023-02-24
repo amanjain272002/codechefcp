99325631322341290656dee24be637ff40a8d115
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-02-24 22:04:46 +0530

    commit

--- concatPal.cpp
+++ concatPal.cpp
@@ -1,44 +1,51 @@
-#include<iostream>
-#include<string>
-#include<map>
-#include<algorithm>
+#include <iostream>
+#include <map>
+#include <string>
 using namespace std;
 
-void solve(){
-    int n,m,count=0;
-    cin>>n>>m;
-    string s1,s2,s;
-    cin>>s1>>s2;
-    s = s1+s2;
-    sort(s.begin(),s.end());
-    for (int i = 1; i < n+m; i++)
-    {
-        if (s[i-1]==s[i])
-        {
-            count++;
-        }
-    }
-    if ((n+m)%2==0 && count >= n+m-1)
-    {
-        cout<<"YES"<<endl;
-    }
-    else if ((n+m)%2!=0 && )
-    {
-        /* code */
-    }
-    
-
-}
-
-
-
-int main(){
+int main()
+{
     int t;
-    cin>>t;
+    cin >> t;
     while (t--)
     {
-        solve();
+        int n, m, count = 0;
+        char c;
+        string ans = "YES";
+        cin >> n >> m;
+        map<char, int> k;
+        for (int i = 0; i < n + m; i++)
+        {
+            cin >> c;
+            k[c]++;
+        }
+        if ((n + m) % 2 == 0)
+        {
+            for (auto it = k.begin(); it != k.end(); it++)
+            {
+                if (it->second % 2 != 0)
+                {
+                    ans = "NO";
+                    break;
+                }
+            }
+        }
+        else
+        {
+            for (auto it = k.begin(); it != k.end(); it++)
+            {
+                if (it->second % 2 != 0)
+                {
+                    count++;
+                }
+            }
+            if (count > 1)
+            {
+                ans = "NO";
+            }
+        }
+        cout << ans << endl;
     }
+
     return 0;
-    
 }
