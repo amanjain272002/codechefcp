513f8e1e4f2f9ddd343936e75a879a6d22a1403e
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-03-04 15:31:33 +0530

    commit

--- cortSent.cpp
+++ cortSent.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
 using namespace std;
+
+void solve()
+{
+    int k;
+    cin >> k;
+    string a;
+    vector<string> v;
+    for (int i = 0; i < k; i++)
+    {
+        cin >> a;
+        v.push_back(a);
+    }
+    int ans = 0;
+    for (int i = 0; i < k; i++)
+    {
+        string s = v[i];
+        int cnt1 = 0, cnt2 = 0;
+        for (int j = 0; j < s.length(); j++)
+        {
+            if (s[j] >= 'a' && s[j] <= 'm')
+            {
+                cnt1++;
+            }
+
+            else if (s[j] >= 'N' && s[j] <= 'Z')
+            {
+                cnt2++;
+            }
+
+            if (cnt1 == s.length() && cnt2 == 0)
+            {
+                ans++;
+            }
+            else if (cnt2 == s.length() && cnt1 == 0)
+            {
+                ans++;
+            }
+        }
+    }
+    if (ans == k)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
 
 int main()
 {
@@ -8,56 +58,7 @@ int main()
     cin >> t;
     while (t--)
     {
-        int k, check = 0, flag = 0;
-        cin >> k;
-        string s;
-        for (int i = 0; i < k; i++)
-        {
-            int flag = 0,check=0;
-            cin >> s;
-            if (s[0] >= 'A' && s[0] <= 'Z')
-            {
-                for (int j = 1; j < s.length(); j++)
-                {
-                    if (s[j] <= 'A' && s[j] >= 'Z')
-                    {
-                        flag = 1;
-                        break;
-                    }
-                    if (s[j] == 'N' || s[j] == 'Z')
-                    {
-                        check = 1;
-                    }
-                }
-            }
-            else
-            {
-                for (int j = 1; j < s.length(); j++)
-                {
-                    if (s[j] <= 'a' && s[j] >= 'z')
-                    {
-                        flag = 1;
-                        break;
-                    }
-                    if (s[j] == 'a' || s[j] == 'm')
-                    {
-                        check = 1;
-                    }
-                }
-            }
-            if (flag == 1)
-            {
-                break;
-            }
-        }
-        if (flag == 0 && check == 1)
-        {
-            cout << "YES" << endl;
-        }
-        else
-        {
-            cout << "NO" << endl;
-        }
+        solve();
     }
 
     return 0;
