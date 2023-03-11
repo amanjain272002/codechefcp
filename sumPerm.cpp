80bf40f918e75dce5494b610dcbbe44679f48612
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-03-11 21:31:03 +0530

    commit

---
+++ sumPerm.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+void solve(){
+    int n;
+    cin>>n;
+    
+}
+
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    solve();
+	}
+	return 0;
+}
