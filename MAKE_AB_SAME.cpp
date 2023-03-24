b7d04e49abbbb0481779b921e2c121044e3d8a80
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-03-24 17:30:53 +0530

    commit

--- MAKE_AB_SAME.cpp
+++ MAKE_AB_SAME.cpp
@@ -10,15 +10,15 @@ int main()
     {
         long int n, countonesA = 0, countonesB = 0;
         cin >> n;
-        char a[n],b[n];
+        char a[n], b[n];
         string ans = "YES";
         for (int i = 0; i < n; i++)
         {
-            cin>>a[i];
+            cin >> a[i];
         }
         for (int i = 0; i < n; i++)
         {
-            cin>>b[i];
+            cin >> b[i];
         }
         for (int i = 0; i < n; i++)
         {
@@ -36,11 +36,17 @@ int main()
         {
             ans = "NO";
         }
+
+        else if ((a[0] != b[0]) || (a[n - 1] != b[n - 1]))
+        {
+            ans = "NO";
+        }
+
         else
         {
-            for (int i = 0; i < n; i++)
+            for (int i = 1; i < n - 1; i++)
             {
-                if (a[i] != b[i] && a[i] == '1')
+                if ((a[i] == '1') && (b[i] == '0'))
                 {
                     ans = "NO";
                     break;
