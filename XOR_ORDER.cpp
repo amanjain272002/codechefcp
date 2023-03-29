17ce7e9b1314521bdace13c2ff355708a1481dea
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-03-29 21:11:25 +0530

    commit

---
+++ XOR_ORDER.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a > b && b > c)
+        {
+            cout << a << endl;
+        }
+        else if (a < b && b < c)
+        {
+            cout << c + 1 << endl;
+        }
+        else
+        {
+            cout << "-1" << endl;
+        }
+    }
+
+    return 0;
+}
