e2824e75e64423e08cbce5fc10103c8d5503d29f
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-04-02 22:19:50 +0530

    commit

--- fibEasy.cpp
+++ fibEasy.cpp
@@ -1,28 +1,67 @@
 #include <iostream>
+#include<math.h>
 using namespace std;
 
-void solve()
-{
+
+// 1 way to solve the quetsion but time limit exceed 
+// void solve()
+// {
+//     long int n;
+//     cin >> n;
+
+//     long int index_save = 0, val = 0, a1 = 0, a2 = 1, a3 = 0, arr[n + 1] = {0};
+//     arr[2] = 1;
+//     for (int i = 3; i <= n; i++)
+//     {
+//         arr[i] = (arr[i - 1] + arr[i - 2]) % 10;
+//     }
+
+//     for (int i = n; i >= 1; i--)
+//     {
+//         if ((i & (~(i - 1))) == i)
+//         {
+//             index_save = i;
+//             break;
+//         }
+//     }
+
+//     cout << arr[index_save] << endl;
+// }
+
+void solve(){
     long int n;
-    cin >> n;
-
-    long int index_save = 0, val = 0, a1 = 0, a2 = 1, a3 = 0, arr[n + 1] = {0};
-    arr[2] = 1;
-    for (int i = 3; i <= n; i++)
+    cin>>n;
+    long int z = log2((long double)n);
+    if (z==0)
     {
-        arr[i] = (arr[i - 1] + arr[i - 2]) % 10;
+        cout<<"0"<<endl;
     }
-
-    for (int i = n; i >= 1; i--)
+    else if (z==1)
     {
-        if ((i & (~(i - 1))) == i)
+        cout<<"1"<<endl;
+    }
+    else
+    {
+        if (z%4==0)
         {
-            index_save = i;
-            break;
+            cout<<"0"<<endl;
         }
+        else if (z%4==2)
+        {
+            cout<<"2"<<endl;
+        }
+        else if (z%4==3)
+        {
+            cout<<"3"<<endl;
+        }
+        else
+        {
+            cout<<"9"<<endl;
+        }
+        
     }
-
-    cout << arr[index_save] << endl;
+    
+    
 }
 
 int main()
