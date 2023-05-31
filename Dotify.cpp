f1bea0481545e82dcb5a9fa83b875ddebb160503
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-05-31 21:13:29 +0530

    commit

---
+++ Dotify.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void doti_play()
+{
+    int n, k, l, flag = 0;
+    cin >> n >> k >> l;
+    vector<int> s;
+    while (n--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (b == l)
+        {
+            s.push_back(a);
+        }
+    }
+    sort(s.begin(), s.end());
+    int sum = 0;
+    for (int i = s.size() - 1; i >= 0; i--)
+    {
+        // cout<<s[i]<<" ";
+        if (k > 0 and s[i] != 0)
+        {
+            flag = 1;
+            sum += s[i];
+            k--;
+        }
+        else
+        {
+            break;
+        }
+    }
+    if (flag == 1)
+    {
+        cout << sum << endl;
+    }
+    else
+    {
+        cout << -1 << endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        doti_play();
+    }
+
+    return 0;
+}
