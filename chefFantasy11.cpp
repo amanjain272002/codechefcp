02f7b1e37680cd7f9b6e7e82ea20e9587154d847
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-11-01 20:32:33 +0530

    commit

---
+++ chefFantasy11.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    cout<<n*(n-1)<<endl;
+    
+}
+
+int main() {
+	int t;
+    cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    
+	return 0;
+}
