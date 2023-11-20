ee82d9614e88e59ebed3d001de985ec062bc8b35
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-11-20 21:37:52 +0530

    commit

---
+++ avgPerm.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        for (int i = 1; i <= n / 2; i++)
+        {
+            cout << n - i + 1 << " " << i << " ";
+        }
+        if (n % 2 != 0)
+        {
+            cout << n / 2 + 1;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
