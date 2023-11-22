63b7a1e3dc0db7569954f4d63076961223fa7ee7
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-11-22 20:03:32 +0530

    commit

---
+++ videoWorth.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int x;
+    cin >> x;
+    cout << x * 1000 * 24 << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
