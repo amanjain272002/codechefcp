c628e2a7e97fc90c7d0c8c9dc2a67c233f19763b
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-11-23 21:05:29 +0530

    commit

---
+++ irstXor.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int c;
+    cin>>c;
+    
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    
+    return 0;
+}
