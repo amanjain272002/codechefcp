e79f5c5da9ca28d8561e7f7783d8092131da5233
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-11-27 21:37:11 +0530

    commit

---
+++ HowManyMax.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    string s;
+    cin>>s;
+    
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    
+    return 0;
+}
